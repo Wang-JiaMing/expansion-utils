ac7da81ad1de212cf088a7e645b68cca873af751
wangjiaming <13422192925@163.com>
2019-05-20 09:54:27 +0800

    添加日期转换工具类

---
+++ src/main/java/com/expansion/change/date/DateUtils.java
@@ -0,0 +1,26 @@
+package com.expansion.change.date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @projectName:expansion-utils
+ * @packageName:com.expansion.change.date
+ * @authorName:wangjiaming
+ * @createDate:2019-05-20
+ * @editor:IntelliJ IDEA
+ * @other:日期转换类
+ **/
+public class DateUtils {
+
+    public static Date strChangeDate(String strDate, String formatStr) {
+        SimpleDateFormat format = new SimpleDateFormat(formatStr);
+        try {
+            Date date = (Date) format.parse(strDate);
+            return date;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
