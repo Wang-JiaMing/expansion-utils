9b9049c328ac9b43a41c57fb9f6e4138dba93abe
wangjiaming <13422192925@163.com>
2019-05-25 17:35:50 +0800

    添加去对象化工具类

--- src/main/java/com/expansion/change/object/ObjectUtils.java
+++ src/main/java/com/expansion/change/object/ObjectUtils.java
@@ -17,31 +17,34 @@ public class ObjectUtils {
 
     /**
      * 去对象化
+     *
      * @param o
      * @return
      */
     public static Map<String, String> ObjectChangeMap(Object o) {
-        Map<String, String> params=new HashMap<String,String>();
+        Map<String, String> params = new HashMap<String, String>();
         Class<?> clazz = o.getClass();
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
-            StringBuffer dataFiledName=new StringBuffer();
-            String[] filedName=field.toString().split(" ")[2].split("\\.");
-            char[] charFiledName =filedName[filedName.length-1].toCharArray();
-            for(int i=0;i<charFiledName.length;i++){
-                if (!Character.isLowerCase(charFiledName[i])){
-                    dataFiledName.append("_"+(char)(charFiledName[i]+32));
-                }else{
+            StringBuffer dataFiledName = new StringBuffer();
+            String[] filedName = field.toString().split(" ")[2].split("\\.");
+            char[] charFiledName = filedName[filedName.length - 1].toCharArray();
+            for (int i = 0; i < charFiledName.length; i++) {
+                if (!Character.isLowerCase(charFiledName[i])) {
+                    dataFiledName.append("_" + (char) (charFiledName[i] + 32));
+                } else {
                     dataFiledName.append(charFiledName[i]);
                 }
             }
-            String tmpGetName=filedName[filedName.length-1];
-            String getNameMethod="get"+(char)(tmpGetName.substring(0,1).toCharArray()[0]-32)+filedName[filedName.length-1].substring(1,tmpGetName.length());
+            String tmpGetName = filedName[filedName.length - 1];
+            String getNameMethod = "get" + (char) (tmpGetName.substring(0, 1).toCharArray()[0] - 32) + filedName[filedName.length - 1].substring(1, tmpGetName.length());
             try {
                 Method methods = clazz.getDeclaredMethod(getNameMethod, null);
                 String value = String.valueOf(methods.invoke(o));
-                params.put(dataFiledName.toString(),value);
-            }catch (Exception e){
+                if (value != null && !"".equals(value)) {
+                    params.put(dataFiledName.toString(), value);
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
