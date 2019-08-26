bdf75aa3fc9d2e881e5c838f071ec71ea2799890
wangjiaming <13422192925@163.com>
2019-08-26 10:41:13 +0800

    排除list、set、map去空格

--- src/main/java/com/expansion/object/ObjectUtils.java
+++ src/main/java/com/expansion/object/ObjectUtils.java
@@ -2,7 +2,9 @@ package com.expansion.object;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -61,7 +63,7 @@ public class ObjectUtils {
         Map<String, Object> params = new HashMap<String, Object>();
         Class<?> clazz = o.getClass();
         Field[] fields = clazz.getDeclaredFields();
-        Field[] fieldSupers=clazz.getSuperclass().getDeclaredFields();
+        Field[] fieldSupers = clazz.getSuperclass().getDeclaredFields();
         for (Field field : fields) {
             StringBuffer dataFiledName = new StringBuffer();
             String[] filedName = field.toString().split(" ")[2].split("\\.");
@@ -71,7 +73,7 @@ public class ObjectUtils {
             try {
                 Method methods = clazz.getDeclaredMethod(getNameMethod, null);
                 String value = String.valueOf(methods.invoke(o));
-                params.put(String.valueOf(charFiledName),value.equals("null")?null:value);
+                params.put(String.valueOf(charFiledName), value.equals("null") ? null : value);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -85,7 +87,7 @@ public class ObjectUtils {
             try {
                 Method methods = clazz.getSuperclass().getDeclaredMethod(getNameMethod, null);
                 String value = String.valueOf(methods.invoke(o));
-                params.put(String.valueOf(charFiledName),value.equals("null")?null:value);
+                params.put(String.valueOf(charFiledName), value.equals("null") ? null : value);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -95,26 +97,34 @@ public class ObjectUtils {
 
     /**
      * 遍历对象所有入参去两边空格
+     *
      * @param o
      * @return
      */
-    public static Object trimMethods(Object o) throws Exception{
-        Class<?> clazz=o.getClass();
+    public static Object trimMethods(Object o) throws Exception {
+        Class<?> clazz = o.getClass();
         Method[] methods = clazz.getDeclaredMethods();
-        for(int i=0;i<methods.length;i++){
-            if(methods[i].getName().indexOf("get")!=-1){
-                String methodsName=methods[i].getName().split("get")[1];
-                String value = String.valueOf(methods[i].invoke(o)).trim();
-                for(int j=0;j<methods.length;j++){
-                    if(methods[j].getName().indexOf("set"+methodsName)!=-1){
-                        methods[j].setAccessible(true);
-                        methods[j].invoke(o,value);
-                        break;
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].getReturnType().getName().indexOf("List") != -1 || methods[i].getReturnType().getName().indexOf("Set") != -1 || methods[i].getReturnType().getName().indexOf("Map") != -1) {
+                if (methods[i].getName().indexOf("get") != -1) {
+                    String methodsName = methods[i].getName().split("get")[1];
+                    String value = String.valueOf(methods[i].invoke(o)).trim();
+                    for (int j = 0; j < methods.length; j++) {
+                        if (methods[j].getName().indexOf("set" + methodsName) != -1) {
+                            methods[j].setAccessible(true);
+                            methods[j].invoke(o, value);
+                            break;
+                        }
                     }
                 }
             }
         }
         return o;
     }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
 
 }
